1ab630ea7911cec497da053851a525919f5964c1
Ray Victor <ray7551@gmail.com>
2024-05-09 20:42:45 +0800

    add fhevmjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // This spreads existing fallbacks
+      'tfhe_bg.wasm': require.resolve('tfhe/tfhe_bg.wasm'),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
